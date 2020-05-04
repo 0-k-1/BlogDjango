70960b2c9c3cc25a7ea942d80aedfad3d8b88f98
geeek0 <geeek0@users.noreply.github.com>
2020-05-05 07:10:49 +0800

    source

--- blog/settings.py
+++ blog/settings.py
@@ -168,9 +168,9 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 # 自定义用户model
 AUTH_USER_MODEL = 'user.Ouser'
 
-SITE_DESCRIPTION = "StormSha的个人网站，记录生活的瞬间，分享学习的心得，感悟生活，留住感动，静静寻觅生活的美好"
+SITE_DESCRIPTION = "MXP的个人网站，记录生活，分享学习"
 
-SITE_KEYWORDS = "StormSha,静觅,网络,IT,技术,博客,Python"
+SITE_KEYWORDS = "Python，学习，生活"
 
 SITE_END_TITLE = "聚会阅读器"
 
